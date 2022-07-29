1c35dc3f17cf30f59b52b0dc9d8a064ea39985f1
Khushi <khushi.kavya6910@gmail.com>
2022-07-29 13:41:40 +0530

    npm-package.json

--- server.js
+++ server.js
@@ -4,11 +4,22 @@
 
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req,res)=>{
     console.log('Request has been made from browser to server');
     // console.log(req.method);
     // console.log(req.url);
+    //lodash
+    let num = _.random(0,20);
+    console.log(num);
+
+    let greet = _.once(()=>{
+        console.log('Hello');
+    });
+
+    greet();
+    greet();
 
     // methods in response object
     // res.setHeader('Content-Type','text/plain');
